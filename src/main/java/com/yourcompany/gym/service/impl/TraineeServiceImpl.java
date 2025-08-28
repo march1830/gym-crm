aa8ca0091d92f3d7302655539fc9d762c7e521f8
Marat Gaisin <maratgaysin76@gmail.com>
2025-08-28 14:24:12 +0400

    feat: алгоритм генерации имени пользователя

--- src/main/java/com/yourcompany/gym/service/impl/TraineeServiceImpl.java
+++ src/main/java/com/yourcompany/gym/service/impl/TraineeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.yourcompany.gym.service.impl;
 
 import com.yourcompany.gym.dao.TraineeDAO;
+import com.yourcompany.gym.dao.TrainerDAO;
 import com.yourcompany.gym.model.Trainee;
 import com.yourcompany.gym.service.TraineeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +14,40 @@ import java.util.Optional;
 public class TraineeServiceImpl implements TraineeService {
 
     private final TraineeDAO traineeDAO;
+    private final TrainerDAO trainerDAO;
 
     @Autowired
-    public TraineeServiceImpl(TraineeDAO traineeDAO) {
+    public TraineeServiceImpl(TraineeDAO traineeDAO, TrainerDAO trainerDAO) {
         this.traineeDAO = traineeDAO;
+        this.trainerDAO = trainerDAO;
     }
 
     @Override
     public Trainee create(Trainee trainee) {
+
+        // TODO: В будущем здесь будет генерация случайного пароля
+        // trainee.setPassword(generateRandomPassword());
+
+        String username = generateUniqueUsername(trainee.getFirstName(), trainee.getLastName());
+        trainee.setUsername(username);
+
         return traineeDAO.save(trainee);
     }
+
+    private String generateUniqueUsername(String firstName, String lastName) {
+        String baseUsername = firstName.toLowerCase() + "." + lastName.toLowerCase();
+        String finalUsername = baseUsername;
+        int counter = 1;
+
+        // Проверяем уникальность среди Trainee и Trainer
+        while (traineeDAO.findByUsername(finalUsername).isPresent() || trainerDAO.findByUsername(finalUsername).isPresent()) {
+            finalUsername = baseUsername + counter;
+            counter++;
+        }
+
+        return finalUsername;
+    }
+
 
     @Override
     public Optional<Trainee> findById(Long id) {
--- src/main/java/com/yourcompany/gym/service/impl/TrainerServiceImpl.java
+++ src/main/java/com/yourcompany/gym/service/impl/TrainerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.yourcompany.gym.service.impl;
 
 import com.yourcompany.gym.dao.TrainerDAO;
+import com.yourcompany.gym.dao.TraineeDAO;
 import com.yourcompany.gym.model.Trainer;
 import com.yourcompany.gym.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +14,38 @@ import java.util.Optional;
 public class TrainerServiceImpl implements TrainerService {
 
     private final TrainerDAO trainerDAO;
+    private final TraineeDAO traineeDAO;
 
     @Autowired
-    public TrainerServiceImpl(TrainerDAO trainerDAO) {
+    public TrainerServiceImpl(TrainerDAO trainerDAO, TraineeDAO traineeDAO) {
         this.trainerDAO = trainerDAO;
+        this.traineeDAO = traineeDAO;
     }
 
     @Override
     public Trainer create(Trainer trainer) {
+
+        // TODO: В будущем здесь будет генерация случайного пароля
+        // trainee.setPassword(generateRandomPassword());
+
+        String username = generateUniqueUsername(trainer.getFirstName(), trainer.getLastName());
+        trainer.setUsername(username);
+
         return trainerDAO.save(trainer);
+    }
+
+    private String generateUniqueUsername(String firstName, String lastName) {
+        String baseUsername = firstName.toLowerCase() + "." + lastName.toLowerCase();
+        String finalUsername = baseUsername;
+        int counter = 1;
+
+        // Проверяем уникальность среди Trainee и Trainer
+        while (trainerDAO.findByUsername(finalUsername).isPresent() || traineeDAO.findByUsername(finalUsername).isPresent()) {
+            finalUsername = baseUsername + counter;
+            counter++;
+        }
+
+        return finalUsername;
     }
 
     @Override
